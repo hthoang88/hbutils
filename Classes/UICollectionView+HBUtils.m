e7f2082948a2764c68f58c114cd60394a28d177c
Hoang Ho <hthoang88@gmail.com>
2019-10-13 20:33:31 +0700

    Init project

---
+++ Classes/UICollectionView+HBUtils.m
@@ -0,0 +1,60 @@
+//
+//  UICollectionView+HBUtils.m
+//  HBUtils
+//
+//  Created by Hoang Ho on 9/29/19.
+//  Copyright © 2019 HB. All rights reserved.
+//
+
+#import "UICollectionView+HBUtils.h"
+
+@implementation UICollectionView (HBUtils)
+- (void)registerCellId:(NSString*)cellId
+{
+    UINib *nib = [UINib nibWithNibName:cellId bundle:nil];
+    [self registerNib:nib forCellWithReuseIdentifier:cellId];
+}
+
+- (void)registerHeaderId:(NSString*)headerId {
+    UINib *nib = [UINib nibWithNibName:headerId bundle:nil];
+    [self registerNib:nib forSupplementaryViewOfKind:UICollectionElementKindSectionHeader withReuseIdentifier:headerId];
+}
+
+- (void)registerEmptyCell {
+    [self registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"BA_EmptyCVC"];
+}
+
+- (UICollectionViewCell *)emptyCell:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [self dequeueReusableCellWithReuseIdentifier:@"BA_EmptyCVC" forIndexPath:indexPath];
+    cell.contentView.backgroundColor = UIColor.clearColor;
+    cell.backgroundColor = UIColor.clearColor;
+    NSLog(@"Empty cell for collection view at %@", indexPath);
+    return cell;
+}
+
+- (UICollectionViewFlowLayout*)flowLayout {
+    UICollectionViewFlowLayout *layout = (id)self.collectionViewLayout;
+    if ([layout isKindOfClass:[UICollectionViewFlowLayout class]]) {
+        return layout;
+    }
+    return nil;
+}
+
+- (BOOL)containIndexPath:(NSIndexPath*)indexPath {
+    if (indexPath.section < self.numberOfSections &&
+        indexPath.row < [self numberOfItemsInSection:indexPath.section]) {
+        return true;
+    }
+    return false;
+}
+@end
+
+@implementation UICollectionViewController (FixCrash)
+- (UIViewController *)previewingContext:(id<UIViewControllerPreviewing>)previewingContext viewControllerForLocation:(CGPoint)location {
+    return nil;
+}
+- (void)previewingContext:(id<UIViewControllerPreviewing>)previewingContext
+     commitViewController:(UIViewController *)viewControllerToCommit {
+    return;
+}
+@end

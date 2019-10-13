899125d007572e2c9976b6330be7b1c8d773861b
Hoang Ho <hthoang88@gmail.com>
2019-10-13 19:37:17 +0700

    Init project

---
+++ HBUtils/HBUtils/HBUtils.h
@@ -0,0 +1,19 @@
+//
+//  HBUtils.h
+//  HBUtils
+//
+//  Created by Hoang Ho on 9/29/19.
+//  Copyright © 2019 HB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HBUtils.
+FOUNDATION_EXPORT double HBUtilsVersionNumber;
+
+//! Project version string for HBUtils.
+FOUNDATION_EXPORT const unsigned char HBUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HBUtils/PublicHeader.h>
+
+
